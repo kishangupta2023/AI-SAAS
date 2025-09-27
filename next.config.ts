9d9659df9d7c75600de5aec0aa048f6d3aa8dcf8
kishan <kishangupta3003@gmail.com>
2025-09-27 14:09:44 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',         // root URL
-        destination: '/home', // redirect to /home
-        permanent: true,     // 301 redirect
-      },
-    ];
-  },
+  /* config options here */
 };
 
 // next.config.js
